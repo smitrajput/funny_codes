686751cc0bd64beaf02d960bce7caebd53ce6d16
Smit Rajput <smitrajputtd@gmail.com>
2019-01-09 19:47:49 +0530

    Connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import config from './config';
 
 let app = express();
+let db = mongoose.connect(config.mongoUrl, { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
