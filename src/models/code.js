9be181d0d79b19982fc19114b2ba778d26de4797
Smit Rajput <smitrajputtd@gmail.com>
2019-01-11 19:13:17 +0530

    Add more code model properties

--- src/models/code.js
+++ src/models/code.js
@@ -5,5 +5,9 @@ let Schema = mongoose.Schema;
 let CodeSchema = new Schema({
     codeType: { type: String, required: true },
     comments: { type: Number, required: true },
-    lahwfs: { type: Number, required: true }
+    lahwfs: { type: Number, required: true },
+    author: { type: String, required: true },
+    language: { type: String, required: true }
 });
+
+module.exports = mongoose.model('Code', CodeSchema);
